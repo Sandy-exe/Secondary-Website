c9b0bd167aa782ce7a93555d7a7015e1aa148d91
sandy-exe <santhoshkumar555001@gmail.com>
2023-09-10 00:18:09 +0530

    Using gpt-3.5-turbo

--- Secondary_website/Fan_box/views.py
+++ Secondary_website/Fan_box/views.py
@@ -5,30 +5,38 @@ from django.http import JsonResponse
 import openai
   
 openai.api_key = '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'
-  
+
+global save_context
+
+save_context =[
+        {"role": "system", "content": "You are Fanbox, who helps to clear doubts related to fiction. "},
+        {"role": "user", "content": "Who are you?"},
+        {"role": "assistant", "content": "I'm a fanbox, who helps fans to clear your doubts related to fiction "}]
+
 def get_completion(prompt):
-    print("ok")
-    print(prompt)
-    query = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.5,
+    global save_context
+    save_context.append({"role": "user", "content": prompt})
+    query = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+        messages=save_context,
     )
-    model_lst = openai.Model.list()
-    for i in model_lst['data']:
-        print(i['id'])
-    response = query.choices[0].text
-    print(response)
+    
+    # model_lst = openai.Model.list()
+    # for i in model_lst['data']:
+    #     print(i['id'])
+    response = query['choices'][0]['message']['content']
+    save_context.append({"role": "assistant", "content": response})
+    print("\n\n")
+    for i in save_context:
+        print(i["role"],":",i["content"])
+    print("\n\n")
     return response
+
   
   
 def query_view(request):
     if request.method == 'POST':
         prompt = request.POST.get('prompt')
         response = get_completion(prompt)
-        print("Response: ",response)
         return JsonResponse({'response': response})
     return render(request, 'index.html')

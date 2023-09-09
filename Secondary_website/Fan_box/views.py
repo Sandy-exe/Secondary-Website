24c8afb6475a1e55422dfde4ae6959a5d2fe09c6
sandy-exe <santhoshkumar555001@gmail.com>
2023-09-09 23:27:46 +0530

    commit all

--- Secondary_website/Fan_box/views.py
+++ Secondary_website/Fan_box/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+from django.views import generic
+from django.http import JsonResponse
+import openai
+  
+openai.api_key = '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'
+  
+def get_completion(prompt):
+    print("ok")
+    print(prompt)
+    query = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    model_lst = openai.Model.list()
+    for i in model_lst['data']:
+        print(i['id'])
+    response = query.choices[0].text
+    print(response)
+    return response
+  
+  
+def query_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        response = get_completion(prompt)
+        print("Response: ",response)
+        return JsonResponse({'response': response})
+    return render(request, 'index.html')
--- Secondary_website/Secondary_website/settings.py
+++ Secondary_website/Secondary_website/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Secondary_website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
